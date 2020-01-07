c7aa08d0581134c563806dadc661254ded834f1a
christinetrac <christinetrac@Christines-MacBook-Air.local>
2020-01-07 18:24:41 -0500

    "i choose you" preloader is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Pokemon from './components/pokemon';
 import './App.css';
+import Spinner from './components/common/Spinner';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
       pokemon: [],
-      loading: false
+      loading: true
     }
   }
 
@@ -19,17 +20,42 @@ class App extends Component {
     .then((data) => {
       this.setState({
         pokemon: data,
-        loading:false
+        //loading:false
       })
+      setTimeout(() => { 
+        this.setState({loading:false})
+      }, 1550);
     })
     .catch(console.log)
   }
 
   render(){
-    return (
+    const pokemon = this.state.pokemon;
+    const loading = this.state.loading;
+    let display;
+    if(pokemon == null || loading){
+      display = <Spinner/>;
+    } else {
+      display = <Pokemon pokemon = {this.state.pokemon}/>;
+    }
+
+    const showPokemon = (
       <div class="Pokecard">
         <Pokemon pokemon = {this.state.pokemon}/>
       </div>
+    );
+
+    const showSpinner = (
+      <div>
+        <Spinner/>
+      </div>
+    );
+
+    return (
+      // <div class="Pokecard">
+      //   <Pokemon pokemon = {this.state.pokemon}/>
+      // </div>
+      <div>{loading ? showSpinner: showPokemon}</div>
     )
   }
 }
---
+++ src/components/common/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import spinner from './choose.gif';
+
+export default () => {
+  return (
+    <div>
+      <img class = "spinner"
+        src={spinner}
+        style={{ height: '155%', margin: 'auto', display: 'block' }}
+        alt="Loading..."
+      />
+    </div>
+  );
+};
