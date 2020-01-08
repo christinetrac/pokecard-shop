efef5d234c15743cf32e196997de6367ed4452f7
christinetrac <root@Christines-MacBook-Air.local>
2020-01-07 21:20:40 -0500

    Updates

---
+++ precache-manifest.b0ad0eccb1c6b45bef8589781780cceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6980fa1488529120265dc62d8c6254",
+    "url": "/pokecard-shop/index.html"
+  },
+  {
+    "revision": "caefb1846d182096e2d6",
+    "url": "/pokecard-shop/static/css/main.792da5b9.chunk.css"
+  },
+  {
+    "revision": "89b6c590bf48afe6c918",
+    "url": "/pokecard-shop/static/js/2.9681418c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokecard-shop/static/js/2.9681418c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "caefb1846d182096e2d6",
+    "url": "/pokecard-shop/static/js/main.5156d332.chunk.js"
+  },
+  {
+    "revision": "17d54d9f0d332a7e1514",
+    "url": "/pokecard-shop/static/js/runtime-main.9a614acb.js"
+  },
+  {
+    "revision": "84c5b716f0f747acc57a8176e3e6affd",
+    "url": "/pokecard-shop/static/media/choose.84c5b716.gif"
+  },
+  {
+    "revision": "e78d3c28fab0578302bb0f33738e50a7",
+    "url": "/pokecard-shop/static/media/pokeball.e78d3c28.gif"
+  }
+]);
